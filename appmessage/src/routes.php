4a9aed817db7404a5af0bf05e800a4d7f15d8b76
charan <charan@contourgroupinc.com>
2016-09-29 16:22:51 +0530

    testing composer

--- appmessage/src/routes.php
+++ appmessage/src/routes.php
@@ -4,3 +4,6 @@ Route::get("readmsg/{id}","Message\MessageController@readmsg");
 Route::get("index",function(){
   return config('message.table');
 });
+Route::get('testcase',function(){
+  return "Hero";
+});
