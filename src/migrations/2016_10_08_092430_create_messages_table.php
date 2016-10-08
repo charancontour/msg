cd6d431fb7f6c536c435dc9d7be46718a455fc41
charan <charan@contourgroupinc.com>
2016-10-08 15:11:42 +0530

    added migrations

---
+++ src/migrations/2016_10_08_092430_create_messages_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMessagesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('messages', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('sender_id')->unsigned();
+			$table->string('receiver')->unsigned();
+			$table->text('message_text');
+			$table->integer('read_status',1);
+			$table->timestamps();
+			$table->foreign('sender_id')->references('id')->on('users');
+			$table->foreign('receiver_id')->references('id')->on('users');
+			
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('messages', function(Blueprint $table)
+		{
+			//
+		});
+	}
+
+}
